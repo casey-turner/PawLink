df1bf3fce6f3f4325e1f27baf5fad1084e6d853c
Casey Turner <casey_turner@outlook.com>
2017-09-13 17:04:13 +1000

    Removed duplicate view folder

--- view/view/includes/hp_footer.php
+++
@@ -1,8 +0,0 @@
-        <footer>
-            <div class="container">
-                <p>© 2017 PawLink.com.au. All Rights Reserved.<p>
-            </div>
-            <script src="view/js/main.js"></script>
-        </footer>
-    </body>
-</html>
